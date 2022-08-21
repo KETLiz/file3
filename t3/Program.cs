946827b55bbc77696ab611a492077e6533f11c39
liza <varlamova_e.t@mail.ru>
2022-08-21 12:24:47 +0300

    Третья задача done

---
+++ t3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Ваше число четное");
+}
+else
+{
+    Console.WriteLine("Ваше число нечетное");
+}
